c898a3e04b23f12b6acb7cbd5fe08c4770aa6b91
Dereyabi <37939640+Dereyabi@users.noreply.github.com>
2019-01-28 23:40:18 +0000

    dsfgsfdg

--- Shader.h
+++ Shader.h
@@ -17,7 +17,7 @@ ID3D11Buffer* CreateConstantBuffer(int size);
 // Load a shader, include the file in the project and pass the name (without the .hlsl extension)
 // to this function. The returned pointer needs to be released before quitting. Returns nullptr on failure
 ID3D11VertexShader* LoadVertexShader(std::string shaderName);
-ID3D11PixelShader*  LoadPixelShader (std::string shaderName);
+ID3D11PixelShader*  LoadPixelShader(std::string shaderName);
 
 // Helper function. Returns nullptr on failure.
 ID3DBlob* CreateSignatureForVertexLayout(const D3D11_INPUT_ELEMENT_DESC vertexLayout[], int numElements);
